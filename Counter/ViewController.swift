d025ead338110fbcd3867bc2a2aad75e5c63015d
mirraMSH <manya0705@yandex.ru>
2023-10-07 14:20:09 +0300

    created base version of click counter app

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var counter = 0
+    
+    @IBAction func pressButtomEffect(_ sender: Any) {
+        counter += 1
 
-
+        countNumber.text = "\(counter)"
+    }
 }
+
+
+
 
